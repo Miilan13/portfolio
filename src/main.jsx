e67310004af5ed6b0aac56a987e18c0f1bde70b2
Solanki Milan <ahirmilanmail@gmail.com>
2025-06-23 22:58:35 +0530

    Update for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </StrictMode>
 );
